428a42d3dc33bff3b9c289344ab0347bb6f402b8
jems <jemsmpatel2005@gmail.com>
2026-03-20 23:36:14 +0530

    Update index.js 3

--- Backend/index.js
+++ Backend/index.js
@@ -65,8 +65,8 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')));
 
     // Any route that is not an API route will be redirected to index.html
-    app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    app.use((req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
     });
 }
 
