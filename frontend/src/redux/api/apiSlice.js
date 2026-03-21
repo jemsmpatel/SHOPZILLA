0bf54777fddc9a4956dc3fbf852fcb30ac0172d2
jems <jemsmpatel2005@gmail.com>
2026-03-21 12:26:28 +0530

    Update apiSlice.js 2

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shopzilla-2ugr.onrender.com/', // Proxy will convert this to http://127.0.0.1:5000
+        baseUrl: process.env.REACT_APP_API_URL,
         credentials: 'include',
     }),
     endpoints: () => ({}), // Extend in other files
